316a50913f7fabcee5ad1f0ff8793cf026f7c433
Krzysztof Koziarski <krzyhook@gmail.com>
2017-04-01 18:08:27 +0200

    Owin packages installed. Startup and ValuesController.

---
+++ WebApiOwinMiddleware/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiOwinMiddleware.Startup))]
+
+namespace WebApiOwinMiddleware
+{
+    using System.Web.Http;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+
+            // Configure Web API Routes:
+            // - Enable Attribute Mapping
+            // - Enable Default routes at /api.
+            httpConfiguration.MapHttpAttributeRoutes();
+            httpConfiguration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
